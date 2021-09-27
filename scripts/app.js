441c58be1890b81ed89453a260b1e6bc2d01164c
dbae82 <dbae82@gmail.com>
2021-09-27 17:22:20 -0500

    feat: got form to send form body to google sheets and set up message after form submission

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,16 @@
 console.log("Welcome to my developers portal. Don't do anything you're not supposed to. Thanks.");
+
+var $form = $('form#contact-form'),
+    url = 'https://script.google.com/macros/s/AKfycbx0kqnAOrFIHZ67hM8L2goOmsW5ACQNIiwDmv01jB6o7mwa74LoFcm3Jf2ObcCcBoO2Jg/exec'
+
+$('#submit-btn').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serialize()
+  })
+  $form.css("display", "none")
+  $('#success').append("<h1>Thank you for reaching out.</h1>")
+})
